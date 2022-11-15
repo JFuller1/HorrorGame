2166b11a049a1f25ec348bd4bc0dd2a6c0174093
erunions <78222901+erunions@users.noreply.github.com>
2022-11-15 10:56:10 -0600

    basic rotation (movement soon:tm:)
    :arrows_counterclockwise: :100:

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    bool movement = false;
+    public Transform tr;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(!movement)
+        {
+            HandleRotation();
+            HandleMovement();
+        }
+    }
+
+    void HandleRotation()
+    {
+        if(Input.GetKeyDown("a"))
+        {
+            Quaternion target = Quaternion.Euler(0, tr.eulerAngles.y - 90f, 0);
+            StartCoroutine(PerformRotation(target));
+        } else if (Input.GetKeyDown("d"))
+        {
+            Quaternion target = Quaternion.Euler(0, tr.eulerAngles.y + 90f, 0);
+            StartCoroutine(PerformRotation(target));
+        }
+    }
+
+    IEnumerator PerformRotation(Quaternion targetRotation)
+    {
+        movement = true;
+        float progress = 0f;
+        float speed = 0.5f;
+        float snap = 0.3f;
+
+        while (progress < 1f)
+        {
+            tr.rotation = Quaternion.Slerp(tr.rotation, targetRotation, progress);
+            progress += Time.deltaTime * speed;
+
+            if (progress <= snap)
+            {
+                yield return null;
+            }
+        }
+        movement = false;
+        yield return null;
+    }
+
+    void HandleMovement()
+    {
+
+    }
+}
