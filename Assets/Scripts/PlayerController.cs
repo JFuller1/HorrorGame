b61d9bc5e7ee267a91b1f6be0d0c7074ecce35be
HeliturtleOP <oproulx8896@gmail.com>
2022-11-16 18:56:53 -0600

    implemented movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,18 @@ public class PlayerController : MonoBehaviour
     bool movement = false;
     public Transform tr;
 
+    Vector3 target;
+    float elapsedTime = 0f;
+    float duration = 3f;
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        if(!movement)
+        if (!movement)
         {
             HandleRotation();
             HandleMovement();
@@ -23,23 +27,49 @@ public class PlayerController : MonoBehaviour
 
     void HandleRotation()
     {
-        if(Input.GetKeyDown("a"))
+        if (Input.GetKeyDown("a"))
         {
-            Quaternion target = Quaternion.Euler(tr.eulerAngles.x, tr.eulerAngles.y - 90f, 0);
+            Quaternion target = Quaternion.Euler(0, tr.eulerAngles.y - 90f, 0);
             StartCoroutine(PerformRotation(target));
-        } else if (Input.GetKeyDown("d"))
+        }
+        else if (Input.GetKeyDown("d"))
         {
-            Quaternion target = Quaternion.Euler(tr.eulerAngles.x, tr.eulerAngles.y + 90f, 0);
+            Quaternion target = Quaternion.Euler(0, tr.eulerAngles.y + 90f, 0);
             StartCoroutine(PerformRotation(target));
         }
     }
+
+    void HandleMovement()
+    {
+
+        if (Input.GetKeyDown("w"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1f))
+            {
+
+                target = hit.transform.position;
+
+                if (hit.transform.CompareTag("Node"))
+                {
+
+                    //movement = true;
+
+                    StartCoroutine(PerformMovement(hit.transform.position));
+                }
+            }
+        }
+
+    }
+
 
     IEnumerator PerformRotation(Quaternion targetRotation)
     {
         movement = true;
         float progress = 0f;
         float speed = 0.5f;
-        float snap = 0.3f;
+        float snap = 0.2f;
 
         while (progress < 1f)
         {
@@ -55,8 +85,21 @@ public class PlayerController : MonoBehaviour
         yield return null;
     }
 
-    void HandleMovement()
+    IEnumerator PerformMovement(Vector3 end)
     {
+        movement = true;
+        float time = 0f;
+        float speed = 5f;
 
+        Vector3 start = transform.position;
+
+        while (time < 1)
+        {
+            transform.position = Vector3.Lerp(start, end, Mathf.SmoothStep(0,1, time));
+            time += Time.deltaTime * speed;
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.05f);
+        movement = false;
     }
 }
