876040a7fbebbbd0c52ce5d976eaa8000989fc8a
Jaeden Fuller <78223047+JFuller1@users.noreply.github.com>
2022-12-08 10:01:47 -0600

    OCCLUSION WORKS BABYYY

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -5,30 +5,87 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
+    [Header("FMOD Event")]
     public EventReference audio;
-    public string sound;
     FMOD.Studio.EventInstance audioEvent;
+
+    Transform slLocation;
+
+    [Header("Occlusion Options")]
+    [Range(0,1)]
+    public float occlusion;
+
+    public LayerMask OcclusionLayer = 1;
+
+    bool isOn;
+    bool soundPlaying;
+
     void Start()
     {
-        audioEvent = FMODUnity.RuntimeManager.CreateInstance(sound);
-        //remove once conditional works
-        audioEvent.start();
-       
+        slLocation = GameObject.FindObjectOfType<StudioListener>().transform;
+        audioEvent = FMODUnity.RuntimeManager.CreateInstance(audio);
     }
 
     void Update()
     {
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(audioEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+
+        if (soundPlaying)
+        {
+            RaycastHit hit;
+            Physics.Linecast(transform.position, slLocation.position, out hit, OcclusionLayer);
+
+            if (hit.collider.name == "Player")
+            {
+                Occlusion(0f);
+                Debug.DrawLine(transform.position, slLocation.position, Color.blue);
+            }
+            else
+            {
+                Occlusion(1f);
+                Debug.DrawLine(transform.position, hit.point, Color.red);
+            }
+        }
     }
 
-    void PlaySound() 
-    { 
-    
-    }
-
-    void StopSound()
+    void Occlusion(float value)
     {
+        audioEvent.setParameterByName("WallMuffle", value);
 
+        float val;
+        audioEvent.getParameterByName("WallMuffle", out val);
+
+        Debug.Log(val);
+    }
+
+    IEnumerator Delay(int delay)
+    {
+        yield return new WaitForSeconds(delay);
+    }
+
+    public void PlaySound(int delay = 0) 
+    {
+        StartCoroutine(Delay(delay));
+
+        audioEvent.start();
+        soundPlaying = true;
+    }
+
+    public void StopSound()
+    {
+        audioEvent.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+    }
+
+    void ToggleSound()
+    {
+        if (soundPlaying == true)
+        {
+            soundPlaying = false;
+        }
+        else
+        {
+            soundPlaying = true;
+        }
     }
 
 }
